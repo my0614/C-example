80e2f3618aa5c34828253184411f49379eaaf61e
Kim minyoung <aqi222@naver.com>
2021-05-26 21:08:00 +0900

    love

---
+++ codeup_1406.c
@@ -0,0 +1,14 @@
+
+#include <stdio.h>
+#include <string.h>
+int main()
+{
+    char str[100];
+    scanf("%s",str);
+    if(strcmp(str,"love") == 0)
+    {
+        printf("I love you.");
+    }
+    return 0;
+}
+
